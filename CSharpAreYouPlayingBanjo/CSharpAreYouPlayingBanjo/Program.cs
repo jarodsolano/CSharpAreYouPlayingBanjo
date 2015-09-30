bb353601ba1bda1616b18d6c65c2e436fbd6b9bb
Jarod Solano <Jarod.Solano@gmail.com>
2015-09-30 08:12:10 -0600

    First Commit to Master

---
+++ CSharpAreYouPlayingBanjo/CSharpAreYouPlayingBanjo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CSharpAreYouPlayingBanjo
+{
+    class Program
+    {
+        public static string AreYouPlayingBanjo(string name)
+        {
+            if (name[0].Equals('r') || name[0].Equals('R'))
+            {
+                return name + " plays banjo";
+            }
+            else
+            {
+                return name + " does not play banjo";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Debug.WriteLine(AreYouPlayingBanjo("ray"));
+        }
+
+    }
+}
